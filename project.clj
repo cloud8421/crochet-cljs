88facf6079380e5f6f8230a7111f24f25a98ede6
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-04-18 08:54:35 +0100

    Adds Reagent and Secretary

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3208"]]
+                 [org.clojure/clojurescript "0.0-3208"]
+                 [reagent "0.5.0"]
+                 [secretary "1.2.3"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :node-dependencies [[source-map-support "0.2.8"]]
   :plugins [[lein-npm "0.4.0"]]
