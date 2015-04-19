96f8a0a974b4431ae24dc99bb12dbd63f27477e7
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-04-19 11:27:45 +0100

    Binds create project form to right components.

--- src/crochet/app_state.cljs
+++ src/crochet/app_state.cljs
@@ -4,8 +4,19 @@
             [crochet.dispatcher :refer [main-publication]])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
+
+(def layout-defaults {:squares [] :width 4 :height 4
+                      :number-of-layers 4 :colors []})
+(defrecord Layout [squares width height
+                   number-of-layers colors])
+
+(def project-defaults {:name "My new project" :layouts []})
+(defrecord Project [name layouts])
+
 (def state (atom {:user {}
-                 :projects []}))
+                 :projects []
+                 :project (map->Project project-defaults)
+                 :layout (map->Layout layout-defaults)}))
 
 (def projects-chan (chan))
 (sub main-publication :projects projects-chan)
--- src/crochet/components/projects.cljs
+++ src/crochet/components/projects.cljs
@@ -2,31 +2,55 @@
   (:require [reagent.core :as reagent]
             [crochet.app-state :refer [state]]))
 
-(defn- name-control []
+(defn- update-layout [key value]
+  (let [layout (:layout @state)
+        updated-layout (assoc layout key value)]
+    (swap! state assoc :layout updated-layout)))
+
+(defn- update-project [key value]
+  (let [project (:project @state)
+        updated-project (assoc project key value)]
+    (swap! state assoc :project updated-project)))
+
+(defn- name-control [project]
   [:div.name-control
    [:label {:for "name"} "Name"]
-   [:input {:type "text" :id "name" :value "My new project"}]])
+   [:input {:type "text"
+            :id "name"
+            :on-change #(update-project :name (-> % .-target .-value))
+            :value (:name project) }]])
 
-(defn- layout-controls []
+(defn- layout-controls [layout]
   [:div.layout-controls
    [:div.width-control
     [:label {:for "width"} "Width"]
-    [:input {:type "number" :id "width" :value 4}]]
+    [:input {:type "number"
+             :id "width"
+             :on-change #(update-layout :width (-> % .-target .-value))
+             :value (:width layout)}]]
    [:div.height-control
     [:label {:for "height"} "Height"]
-    [:input {:type "number" :id "height" :value 4}]]
+    [:input {:type "number"
+             :id "height"
+             :on-change #(update-layout :height (-> % .-target .-value))
+             :value (:height layout)}]]
    [:div.number-of-layers-control
     [:label {:for "number-of-layers"} "Layers"]
-    [:input {:type "number" :id "number-of-layers" :value 3}]]
+    [:input {:type "number"
+             :id "number-of-layers"
+             :on-change #(update-layout :number-of-layers (-> % .-target .-value))
+             :value (:number-of-layers layout)}]]
    [:button {:id "generate"} "Generate"]])
 
 (defn add-new-project-component []
-  [:section.new-project
-   [:section.workspace
-    [:section.editor
-     [name-control]
-     [:h2 "Dimensions"]
-     [layout-controls]]]])
+  (let [project (:project @state)
+        layout (:layout @state)]
+    [:section.new-project
+     [:section.workspace
+      [:section.editor
+       [name-control project]
+       [:h2 "Dimensions"]
+       [layout-controls layout]]]]))
 
 (defn project-component [project]
   [:li (:name project)])
