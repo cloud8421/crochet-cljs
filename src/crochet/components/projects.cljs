37d57d65a990c01e45cc1d1c0e7e2a81536699e3
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-04-19 10:21:23 +0100

    Adds dumb new project form.

--- src/crochet/components/projects.cljs
+++ src/crochet/components/projects.cljs
@@ -2,8 +2,31 @@
   (:require [reagent.core :as reagent]
             [crochet.app-state :refer [state]]))
 
+(defn- name-control []
+  [:div.name-control
+   [:label {:for "name"} "Name"]
+   [:input {:type "text" :id "name" :value "My new project"}]])
+
+(defn- layout-controls []
+  [:div.layout-controls
+   [:div.width-control
+    [:label {:for "width"} "Width"]
+    [:input {:type "number" :id "width" :value 4}]]
+   [:div.height-control
+    [:label {:for "height"} "Height"]
+    [:input {:type "number" :id "height" :value 4}]]
+   [:div.number-of-layers-control
+    [:label {:for "number-of-layers"} "Layers"]
+    [:input {:type "number" :id "number-of-layers" :value 3}]]
+   [:button {:id "generate"} "Generate"]])
+
 (defn add-new-project-component []
-  [:h2 "Add new project here"])
+  [:section.new-project
+   [:section.workspace
+    [:section.editor
+     [name-control]
+     [:h2 "Dimensions"]
+     [layout-controls]]]])
 
 (defn project-component [project]
   [:li (:name project)])
