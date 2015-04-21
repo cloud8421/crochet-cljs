33560fd12416b59d993a7ef267c31dccee4db8a2
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-04-21 12:06:45 +0100

    Total is a string, not a function.

--- src/crochet/components/history.cljs
+++ src/crochet/components/history.cljs
@@ -16,7 +16,7 @@
   (let [revisions (:revisions history)]
     [:section.history
      [:header
-      [:span.count (str (count revisions) total)]
+      [:span.count (str (count revisions) " total")]
       [:h2 "History"]]
      (if (> (count revisions) 0)
        [:ul
